8edd6cf0c7345014a3ff681f118e81fb92e6b5cf
mitch <mitchellchai2000@gmail.com>
2024-01-28 12:59:30 +0000

    Linked up buttons and scenes

--- Assets/Scripts/SceneChanger.cs
+++ Assets/Scripts/SceneChanger.cs
@@ -7,14 +7,16 @@ using UnityEngine.UI;
 public class SceneChanger : MonoBehaviour
 {
     // buttons
-    [SerializeField] Button mainMenuButton, startGameButton, exitGameButton;
+    [SerializeField] Button mainMenuButton, helpButton, creditsButton, playButton, exitButton;
 
     // start function
     public void Start()
     {
         mainMenuButton.onClick.AddListener(MainMenu);
-        startGameButton.onClick.AddListener(LevelOne);
-        exitGameButton.onClick.AddListener(Quit);
+        helpButton.onClick.AddListener(Help);
+        creditsButton.onClick.AddListener(Credits);
+        playButton.onClick.AddListener(LevelOne);
+        exitButton.onClick.AddListener(Quit);
     }
 
 
@@ -23,16 +25,34 @@ public class SceneChanger : MonoBehaviour
     {
         SceneManager.LoadScene(0);
     }
-    public void LevelOne()
+
+    public void Help()
     {
         SceneManager.LoadScene(1);
-
     }
-    public void LevelTwo()
+
+    public void Credits()
     {
         SceneManager.LoadScene(2);
+    }
+    public void Clear()
+    {
+        SceneManager.LoadScene(3);
 
     }
+
+    public void Fail()
+    {
+        SceneManager.LoadScene(4);
+
+    }
+
+    public void LevelOne()
+    {
+        SceneManager.LoadScene(5);
+
+    }
+    
     public void Quit()
     {
         Application.Quit();
