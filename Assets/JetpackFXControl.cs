25145df7887cc06790ab83b2dd880fd1304d00d3
Jock Findlay <jock@haikuinteractive.com>
2024-01-28 12:16:55 +0000

    Jetpack particle effects 2/2

--- Assets/JetpackFXControl.cs
+++ Assets/JetpackFXControl.cs
@@ -1,23 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
+using StarterAssets;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class JetpackFXControl : MonoBehaviour
 {
-    private ParticleSystem leftJetPack;
-    private ParticleSystem righJetPack;
+    private List<ParticleSystem> jetPackVFXs = new List<ParticleSystem>();
+    private float emissionRate = 0f;
+    private StarterAssetsInputs input;
 
-    // Start is called before the first frame update
     void Start()
     {
-        var particleSystems = gameObject.GetComponents<ParticleSystem>();
+        var particleSystems = gameObject.GetComponentsInChildren<ParticleSystem>();
 
-        Debug.LogWarning("particles" + particleSystems.Length);
+        foreach (var particleSystem in particleSystems) {
+            if (particleSystem.name.StartsWith("Jetpack")) {
+                jetPackVFXs.Add(particleSystem);
+                particleSystem.Play();
+                var emission = particleSystem.emission;
+                emission.rateOverTime = 0f;
+            }
+        }
+
+        input = GetComponent<StarterAssetsInputs>();
     }
 
-    // Update is called once per frame
+    const float MaxParticleRate = 300f;
+    const float ParticleEmissionLerpFactor = 10f;
+
     void Update()
     {
+        float inputMagnitude = input.move == Vector2.zero ? 0f : input.analogMovement ? input.move.magnitude : 1f;
+
+        emissionRate = Mathf.Lerp(emissionRate, inputMagnitude, Time.deltaTime * ParticleEmissionLerpFactor);
         
+        foreach (var jetPack in jetPackVFXs) {
+            var emission = jetPack.emission;
+
+            emission.rateOverTime = emissionRate * MaxParticleRate;
+        }
     }
 }
